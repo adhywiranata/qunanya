060c646185c7c6b503fca28875557f386a2a9b71
Adhy <adhywiranatap@gmail.com>
2017-02-18 22:17:47 +0700

    added navbar spec

---
+++ __tests__/components/navbar.spec.jsx
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Navbar from '../../src/components/Navbar';
+
+describe('<Navbar>', () => {
+  it('renders correctly', () => {
+    const navbar = shallow(<Navbar />);
+    expect(navbar).toBeDefined();
+  });
+});
