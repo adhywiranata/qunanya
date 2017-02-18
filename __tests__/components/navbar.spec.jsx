aba53ad1a2a6062877dadb405749287be41f8c42
Adhy <adhywiranatap@gmail.com>
2017-02-18 22:54:25 +0700

    updated navbar spec

--- __tests__/components/navbar.spec.jsx
+++ __tests__/components/navbar.spec.jsx
@@ -7,4 +7,9 @@ describe('<Navbar>', () => {
     const navbar = shallow(<Navbar />);
     expect(navbar).toHaveLength(1);
   });
+
+  it('renders 4 menus', () => {
+    const navbar = shallow(<Navbar />);
+    expect(navbar.find('TouchableHighlight')).toHaveLength(4);
+  });
 });
