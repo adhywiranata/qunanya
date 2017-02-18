31b9bf3bb08dc82624b309448f79195f8a5a3bf4
Adhy <adhywiranatap@gmail.com>
2017-02-18 23:17:54 +0700

    updated questionPostBox spec

--- __tests__/components/questionPostBox.spec.jsx
+++ __tests__/components/questionPostBox.spec.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import QuestionPostBox from '../../src/components/QuestionPostBox';
+
 describe('<QuestionPostBox>', () => {
   it('renders correctly', () => {
     const questionPostBox = shallow(<QuestionPostBox />);
     expect(questionPostBox).toHaveLength(1);
+  });
+
+  it('renders a TextInput', () => {
+    const questionPostBox = shallow(<QuestionPostBox />);
+    expect(questionPostBox.find('TextInput')).toHaveLength(1);
   });
 });
