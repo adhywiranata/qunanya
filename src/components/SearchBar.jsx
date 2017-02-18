ae4834d5d056fbf7a60404f6cd54358594828eb9
Adhy <adhywiranatap@gmail.com>
2017-02-18 22:30:27 +0700

    added search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SearchBar = () => (
+  <View>
+    <Text>
+      test
+    </Text>
+  </View>
+);
+
+export default SearchBar;
