200835b6eac2ac48ffedd46bb579bcb30b4fb0aa
Adhy <adhywiranatap@gmail.com>
2017-02-19 22:23:02 +0700

    added question post box component

---
+++ src/components/QuestionPostBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, TextInput } from 'react-native';
+
+const QuestionPostBox = () => (
+  <View>
+    <TextInput />
+  </View>
+);
+
+export default QuestionPostBox;
