b23e704902ff0ce982324f5265b7d997611c9259
Adhy <adhywiranatap@gmail.com>
2017-02-19 22:25:57 +0700

    added MenuBar component

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, TouchableHighlight } from 'react-native';
+
+const MenuBar = () => (
+  <View>
+    <TouchableHighlight>Feed</TouchableHighlight>
+    <TouchableHighlight>Bookmark</TouchableHighlight>
+  </View>
+);
+
+export default MenuBar;
