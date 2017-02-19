ec14aa33d68c91a2102ada65195b7a3fedb0b2e3
Adhy <adhywiranatap@gmail.com>
2017-02-19 22:38:56 +0700

    updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 
 import SearchBar from './SearchBar';
+import MenuBar from './MenuBar';
 
 const Header = () => (
   <View>
     <SearchBar />
+    <MenuBar />
   </View>
 );
 
