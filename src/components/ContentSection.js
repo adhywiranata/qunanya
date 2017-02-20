3c6846e9a13f950943ca638b20ddff9295fe3462
Adhy <adhywiranatap@gmail.com>
2017-02-20 16:14:30 +0700

    updated question post box component inside content section

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 
 import styles from '../styles';
 
+import QuestionPostBox from './QuestionPostBox';
 import ContentItem from './ContentItem';
 
 const ContentSection = () => (
   <ScrollView style={styles.contentSection}>
+    <QuestionPostBox />
     { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => <ContentItem key={item} />) }
   </ScrollView>
 );
--- src/components/QuestionPostBox.js
+++ src/components/QuestionPostBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
 
+import styles from '../styles';
+
 const QuestionPostBox = () => (
-  <View styles={styles.questionPostBox}>
-    <TextInput styles={styles.questionPostInput} />
+  <View style={styles.questionPostBox}>
+    <TextInput
+      style={styles.questionPostInput}
+      placeholder="What's your question?"
+    />
   </View>
 );
 
