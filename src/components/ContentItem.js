cd61ee1c39524c9d3cde2d95d05bef80ab531fec
Adhy <adhywiranatap@gmail.com>
2017-02-20 17:53:33 +0700

    added content item component

--- src/components/ContentItem.js
+++ src/components/ContentItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 import styles from '../styles';
 
 const ContentItem = () => (
   <View style={styles.contentItem}>
-    <Text>test</Text>
+    <Text>
+      Kenapa React lebih populer ketimbang Angular?
+    </Text>
+    <View>
+      <Text>MUKA ORANG</Text>
+      <Text>Jimminy Cricket, Software Engineer, Lapakpedia</Text>
+      <Text>Written Feb 10. Upvoted by Rudi, Mike, John, and 213 others</Text>
+    </View>
+    <View>
+      <Text>
+        Sebenarnya, React tidak bisa dibandingkan apple-to-apple dengan Angular.
+        Namun, bla bla bla bla.
+        <TouchableHighlight>
+          <Text>Read More..</Text>
+        </TouchableHighlight>
+      </Text>
+    </View>
   </View>
 );
 
