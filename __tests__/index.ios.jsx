56cabe1dd820aa24fbb1a34af592747af94db618
Adhy <adhywiranatap@gmail.com>
2017-02-20 11:40:45 +0700

    updated rn index spec

--- __tests__/index.ios.jsx
+++ __tests__/index.ios.jsx
@@ -1,14 +1,18 @@
 import 'react-native';
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import Index from '../index.ios';
 
 describe('React renderer', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(
-      <Index />,
-    );
-    expect(tree).toBeDefined();
+    const wrapper = shallow(<Index />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('renders <App />', () => {
+    const wrapper = shallow(<Index />);
+    expect(wrapper.find('<App />')).toHaveLength(1);
   });
 });
